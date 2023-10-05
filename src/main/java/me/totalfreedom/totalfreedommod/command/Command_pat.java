7eaaf2d1ad80f11291e58202a8d17a889c9babe7
0x7694C9 <opsimulation@gmail.com>
2023-10-05 17:20:32 -0400

    Create Emote commands
    I might just replace this with EmotesTF if it doesn't work.

---
+++ src/main/java/me/totalfreedom/totalfreedommod/command/Command_pat.java
@@ -0,0 +1,31 @@
+package me.totalfreedom.totalfreedommod.command;
+
+import me.totalfreedom.totalfreedommod.rank.Rank;
+import me.totalfreedom.totalfreedommod.util.FUtil;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandPermissions(level = Rank.OP, source = SourceType.ONLY_IN_GAME)
+@CommandParameters(description = "Give someone a head pat uwu", usage = "/<command> <player>")
+public class Command_pat extends FreedomCommand
+{
+
+    @Override
+    public boolean run(CommandSender sender, Player playerSender, Command cmd, String commandLabel, String[] args, boolean senderIsConsole)
+    {
+        if (args.length == 0)
+        {
+            return false;
+        }
+        Player player = getPlayer(args[0]);
+        if (player == null)
+        {
+            msg(FreedomCommand.PLAYER_NOT_FOUND);
+            return true;
+        }
+        FUtil.bcastMsg(ChatColor.AQUA + (playerSender.getName()) + ChatColor.AQUA + " gave " + (player.getName()) + ChatColor.AQUA + " a pat on the head");
+        return true;
+    }
+}
